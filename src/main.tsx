31db43720089a73a1e19b5371d286143f9e4309c
Aaron Dake <aaron.dake@wwwinc.com>
2025-04-30 16:53:16 -0400

    Utilize provider.tsx
    Using the recommended code for main.tsx now, utilizing provider.tsx which was created with the snippet add command.
    
    Also adding @ rule to the tsconfig path.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import { ThemeProvider } from "next-themes"
+import { Provider } from "@/components/ui/provider"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
-      <ThemeProvider attribute="class" disableTransitionOnChange>
-        <App />
-      </ThemeProvider>
-    </ChakraProvider>
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
